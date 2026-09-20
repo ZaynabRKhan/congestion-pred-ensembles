b73e0c020731b8cbd3021867bf4fbd0cf0c00aad
Zaynab <zaynabrkhan@gmail.com>
2026-09-20 11:37:46 +0530

    Added the NLL Loss

--- README.md
+++
@@ -1,2 +0,0 @@
-# congestion-pred-ensembles
-This project uses ensembles to explore predictive uncertainty and how well it reflects the model error in congestion prediction using CircuitNet
---
+++ models/__init__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 CircuitNet. All rights reserved.
+
+from .gpdl import GPDL
+# from .routenet import RouteNet
+# from .mavi import MAVI
+
+
+# __all__ = ['GPDL', 'RouteNet', 'MAVI']
+__all__ = ['GPDL']
+
+'''
+===> Avg. NRMS: 0.0460
+===> Avg. SSIM: 0.7769
+===> Avg. EMD: 0.0039
+'''
